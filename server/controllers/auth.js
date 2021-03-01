ed55d1b4350dd78b01f58588e0b9d391b8ae66a9
Manuela Garcia <mgh9517@gmail.com>
2021-03-01 16:31:16 -0500

    Add user model and add user to mongo db

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,22 @@
-export const showMessage = (req, res) => {
-    res.status(200).send(req.params.message)
-};
+import User from '../models/user.js';
 
 export const register = async (req, res) => {
-    console.log(req.body);
+    const {name, password, email} = req.body;
+
+    if(!name) return res.status(400).send('Name is required')
+    if(!password || password.length < 6) return res.status(400).send('Password is required and should be min 6 characters')
+    
+    let isEmailInDB = await User.findOne({ email }).exec();
+    if (isEmailInDB) return res.status(400).send('Email is taken')
+
+    const newUser = User(req.body);
+
+    try {
+        await newUser.save()
+        console.log('User created', newUser);
+        return res.json({ ok: true });
+    } catch (error) {
+        console.log('Create user failed: ', err);
+        return res.status(400).send('Error. Try again.');
+    }
 };
